60cb605cd63ec8a7fc4cccee173a60b9501f554b
emiridbest <udogwuemiri@gmail.com>
2023-08-22 02:30:02 +0100

    third

---
+++ contracts/PetShop.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract PetShop {
+    struct Pet {
+        uint id;
+        uint8 age;
+        string name;
+        string breed;
+        string color;
+        string description;
+        string CID;
+        bool adopted;
+        address adopter;
+    }
+
+    Pet[] public petList;
+    mapping(uint => address) public owner;
+
+    event PetUploaded(
+        uint id,
+        uint8 age,
+        string name,
+        string breed,
+        string color,
+        string description,
+        string CID
+    );
+    event PetAdopted(uint256 indexed petId, address indexed adopter);
+
+    function addPet(
+        uint8 age,
+        string memory name,
+        string memory breed,
+        string memory color,
+        string memory description,
+        string memory CID
+    ) external {
+        uint petId = petList.length;
+        petList.push(
+            Pet(
+                petId,
+                age,
+                name,
+                breed,
+                color,
+                description,
+                CID,
+                false,
+                address(0)
+            )
+        );
+        owner[petId] = msg.sender;
+        emit PetUploaded(petId, age, name, breed, color, description, CID);
+    }
+
+    function adoptPet(uint256 petId) public {
+        require(petId >= 0 && petId <= petList.length, "Pet does not exist");
+        require(!petList[petId].adopted, "Pet already adopted");
+        require(msg.sender != owner[petId], "You already own this pet");
+        petList[petId].adopted = true;
+          petList[petId].adopter = msg.sender;
+
+        emit PetAdopted(petId, msg.sender);
+    }
+
+    function getAvailablePets() external view returns (uint256[] memory) {
+        uint256[] memory availablePetIds = new uint256[](petList.length);
+        uint256 count = 0;
+
+        for (uint256 i = 0; i < petList.length; i++) {
+            if (!petList[i].adopted) {
+                availablePetIds[count] = i + 1;
+                count++;
+            }
+        }
+
+        return availablePetIds;
+    }
+
+    function adoptedPets() external view returns (uint256[] memory) {
+        uint256[] memory adoptedPetIds = new uint256[](petList.length);
+        uint256 count = 0;
+
+        for (uint256 i = 0; i < petList.length; i++) {
+            if (petList[i].adopted) {
+                adoptedPetIds[count] = i + 1;
+                count++;
+            }
+        }
+
+        return adoptedPetIds;
+    }
+
+    function getPetInfo(uint256 petId) external view returns (Pet memory) {
+        require(petId >= 0 && petId <= petList.length, "Pet does not exist");
+        return petList[petId - 1];
+    }
+}
